8f3e4731811f298866db2256d0116fe0827046b8
Liam Hemo <liamhemo64@gmail.com>
2025-12-13 22:14:41 +0200

    deleted comments

--- routes/postRoutes.ts
+++ routes/postRoutes.ts
@@ -2,8 +2,6 @@ import express from "express";
 const router = express.Router();
 import postController from "../controllers/postController.ts";
 
-console.log("✅ PostRoutes file is loaded!"); // הדפסה בעליית השרת
-
 router.post("/", postController.create.bind(postController));
 
 export default router;
