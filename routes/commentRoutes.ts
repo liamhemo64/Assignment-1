4b810810888d6a683006975b1e5368d7afdda03c
Liam Hemo <liamhemo64@gmail.com>
2025-12-14 21:47:43 +0200

    added generic update function that use PUT, the update function required a id of the post or comment

--- routes/commentRoutes.ts
+++ routes/commentRoutes.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import commentController from "../controllers/commentController.ts";
 
 router.post("/", commentController.create.bind(commentController));
+router.put("/:id", commentController.update.bind(commentController));
 
 export default router;
