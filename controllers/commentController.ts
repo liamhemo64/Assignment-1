b2f9f152e29f965de12273cccabd27981a83a0a5
annaeden1 <annaeden1@gmail.com>
2025-12-17 20:14:51 +0200

    add delete function in controller and route and request in request.rest

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -1,9 +1,25 @@
 import { commentModel, type IComment } from "../models/commentModel.ts";
 import genericController from "./genericController.ts";
+import type { Request, Response } from "express";
 
 class commentController extends genericController<IComment> {
   constructor() {
     super(commentModel);
+  }
+  
+  async delete(req: Request, res: Response) {
+    const id = req.params.id;
+    try {
+      const response = await this.model.findOneAndDelete({ id });
+
+      if (!response) {
+        return res.status(404).json({ error: `Item with id ${id} not found` });
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error instanceof Error ? error.message : 'An unknown error occurred' });
+    }
   }
 }
 
--- routes/commentRoutes.ts
+++ routes/commentRoutes.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import commentController from "../controllers/commentController.ts";
 
 router.post("/", commentController.create.bind(commentController));
+router.delete("/:id", commentController.delete.bind(commentController));
 
 export default router;
